536c3f4055607aedac160520e5fbfeb0a53d167a
tracer-ics2017 <tracer@njuics.org>
2026-04-02 17:48:26 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:48:26 up 15:48,  3 users,  load average: 0.17, 0.11, 0.04 e88c8152e592375a8150261e6ad1296ed44780db

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -3,63 +3,45 @@
 
 #include "common.h"
 
+/* 寄存器索引枚举 */
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
+/* 分层定义1：单个通用寄存器的联合体（32/16/8位重叠） */
 typedef union {
+  uint32_t _32;
+  uint16_t _16;
+  uint8_t _8[2];
+} GPR;
+
+/* 分层定义2：CPU状态结构体 */
+typedef struct {
+  /* 8个通用寄存器 */
   union {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
-
-  struct {
-    uint32_t eax;
-    uint32_t ecx;
-    uint32_t edx;
-    uint32_t ebx;
-    uint32_t esp;
-    uint32_t ebp;
-    uint32_t esi;
-    uint32_t edi;
+    GPR gpr[8];
+    struct { uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi; };
+    struct { uint16_t ax, cx, dx, bx, sp, bp, si, di; };
+    struct { uint8_t al, ah, cl, ch, dl, dh, bl, bh; };
   };
-
-  struct {
-    uint16_t ax;
-    uint16_t cx;
-    uint16_t dx;
-    uint16_t bx;
-    uint16_t sp;
-    uint16_t bp;
-    uint16_t si;
-    uint16_t di;
-  };
-
-  struct {
-    uint8_t al; uint8_t ah;
-    uint8_t cl; uint8_t ch;
-    uint8_t dl; uint8_t dh;
-    uint8_t bl; uint8_t bh;
-  };
-
+  /* 独立的指令指针EIP（不与通用寄存器重叠） */
   vaddr_t eip;
 } CPU_state;
 
 extern CPU_state cpu;
 
+/* 寄存器访问宏 */
 #define reg_l(index) (cpu.gpr[index]._32)
 #define reg_w(index) (cpu.gpr[index]._16)
 #define reg_b(index) (cpu.gpr[index & 0x3]._8[index >> 2])
 
+/* 寄存器名称数组声明 */
 extern const char* regsl[];
 extern const char* regsw[];
 extern const char* regsb[];
 
+/* 寄存器名称获取函数 */
 static inline const char* reg_name(int index, int width) {
-  extern const char* regsl[];
-  extern const char* regsw[];
-  extern const char* regsb[];
   switch (width) {
     case 4: return regsl[index];
     case 1: return regsb[index];
