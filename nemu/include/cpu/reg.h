681b9d480f4a24c26ef2d078ab4983c8ad985c13
tracer-ics2017 <tracer@njuics.org>
2026-04-02 17:34:08 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:34:08 up 15:33,  3 users,  load average: 0.07, 0.04, 0.00 8be7905e26965d7392a00476e408bdcbca36ca0b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -1,60 +1,90 @@
-#ifndef __REG_H__
-#define __REG_H__
+#ifndef __CPU_REG_H__
+#define __CPU_REG_H__
 
 #include "common.h"
+/* 寄存器索引枚举 */
+enum {
+      R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI,
+          NR_REG,
+          
+};
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum {
+      R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH,
+      
+};
 
-/* TODO: Re-organize the `CPU_state' structure to match the register
- * encoding scheme in i386 instruction format. For example, if we
- * access cpu.gpr[3]._16, we will get the `bx' register; if we access
- * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
- * For more details about the register encoding scheme, see i386 manual.
- */
-
-typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
+enum {
+      R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI,
+      
+};
+/* 通用寄存器联合体：实现32/16/8位寄存器的硬件重叠映射 */
+typedef union {
+  union {
+            uint32_t _32;
+            uint16_t _16;
+            uint8_t _8;
+                                
   } gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+      /* 32位通用寄存器别名，对应i386的8个32位GPR */
+  struct {
+            uint32_t eax;
+            uint32_t ecx;
+            uint32_t edx;
+            uint32_t ebx;
+            uint32_t esp;
+            uint32_t ebp;
+            uint32_t esi;
+            uint32_t edi;
+                                                                        
+  };
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      /* 16位通用寄存器别名，对应32位寄存器的低16位 */
+  struct {
+            uint16_t ax;
+            uint16_t cx;
+            uint16_t dx;
+            uint16_t bx;
+            uint16_t sp;
+            uint16_t bp; 
+            uint16_t si;
+            uint16_t di;
+                                                                        
+  };
 
-  vaddr_t eip;
+  /* 8位通用寄存器别名，对应16位寄存器的高8位/低8位 */
+  struct {
+            uint8_t al;
+            uint8_t ah;
+            uint8_t cl;
+            uint8_t ch;
+            uint8_t dl;
+            uint8_t dh;
+            uint8_t bl;
+            uint8_t bh;
+                                                                        
+  };
 
+      /* 指令指针寄存器EIP，核心的程序计数器 */
+      vaddr_t eip;
+
+      
 } CPU_state;
 
 extern CPU_state cpu;
 
-static inline int check_reg_index(int index) {
-  assert(index >= 0 && index < 8);
-  return index;
-}
+/* 寄存器访问宏，与框架代码的reg_test()完全匹配 */
+#define reg_l(index) (cpu.gpr[index]._32)
+#define reg_w(index) (cpu.gpr[index]._16)
+#define reg_b(index) (cpu.gpr[index]._8)
 
-#define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
-#define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
-#define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
-
+/* 寄存器名称数组，用于后续info r命令打印 */
 extern const char* regsl[];
 extern const char* regsw[];
 extern const char* regsb[];
-
-static inline const char* reg_name(int index, int width) {
-  assert(index >= 0 && index < 8);
-  switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
-  }
-}
+/* reg_name 函数声明 */
+const char* reg_name(int index, int width);
 
 #endif
+
