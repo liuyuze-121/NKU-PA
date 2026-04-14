1f1287a34686947a7fb6a1d52a0444608c013669
tracer-ics2017 <tracer@njuics.org>
2026-04-14 22:59:41 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:59:41 up 23 min,  2 users,  load average: 0.46, 0.41, 0.19 70a63813f5c4d3ccd70107ac1016502c54491cc6

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,29 +7,31 @@ enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
+/* TODO: Re-organize the `CPU_state' structure to match the register
+ * encoding scheme in i386 instruction format. For example, if we
+ * access cpu.gpr[3]._16, we will get the `bx' register; if we access
+ * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
+ * For more details about the register encoding scheme, see i386 manual.
+ */
+
 typedef struct {
   union {
     struct {
-      union {
-        uint32_t _32;
-        uint16_t _16;
-        uint8_t _8[2];
-      } gpr[8];
-      
-      struct {
-        uint32_t eax;
-        uint32_t ecx;
-        uint32_t edx;
-        uint32_t ebx;
-        uint32_t esp;
-        uint32_t ebp;
-        uint32_t esi;
-        uint32_t edi;
-      };
-    };
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
+
+    /* Do NOT change the order of the GPRs' definitions. */
+
+    /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+     * in PA2 able to directly access these registers.
+     */
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
   };
 
   vaddr_t eip;
+
 } CPU_state;
 
 extern CPU_state cpu;
