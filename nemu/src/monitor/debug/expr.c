56e98043e7996fc577dbedc0b8afb311b58d54f7
tracer-ics2017 <tracer@njuics.org>
2026-04-15 16:51:44 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:51:44 up  4:36,  3 users,  load average: 0.29, 0.08, 0.02 827754cde55145c934f33c836e9920faa7152dd

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,39 +1,47 @@
 #include "nemu.h"
-
-/* We use the POSIX regex functions to process regular expressions.
- * Type 'man regex' for more information about POSIX regex functions.
- */
 #include <sys/types.h>
 #include <regex.h>
+#include <string.h>
+#include <assert.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
-
-  /* TODO: Add more token types */
-
+  TK_NOTYPE = 256,
+  TK_NUMBER,
+  TK_HEX,
+  TK_REG,
+  TK_EQ,
+  TK_NEQ,
+  TK_AND,
+  TK_OR,
+  TK_NEGATIVE,
+  TK_DEREF
 };
 
 static struct rule {
   char *regex;
   int token_type;
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {" +", TK_NOTYPE},
+  {"0x[1-9A-Fa-f][0-9A-Fa-f]*", TK_HEX},
+  {"[0-9][0-9]*", TK_NUMBER},
+  {"\\$(eax|ecx|edx|ebx|esp|ebp|esi|edi|eip|ax|cx|dx|bx|sp|bp|si|di|al|cl|dl|bl|ah|ch|dh|bh)", TK_REG},
+  {"==", TK_EQ},
+  {"!=", TK_NEQ},
+  {"&&", TK_AND},
+  {"\\|\\|", TK_OR},
+  {"!", '!'},
+  {"\\+", '+'},
+  {"-", '-'},
+  {"\\*", '*'},
+  {"\\/", '/'},
+  {"\\(", '('},
+  {")", ')'}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
 static regex_t re[NR_REGEX];
 
-/* Rules are used for many times.
- * Therefore we compile them only once before any usage.
- */
 void init_regex() {
   int i;
   char error_msg[128];
@@ -64,7 +72,6 @@ static bool make_token(char *e) {
   nr_token = 0;
 
   while (e[position] != '\0') {
-    /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
@@ -74,15 +81,30 @@ static bool make_token(char *e) {
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-
-        switch (rules[i].token_type) {
-          default: TODO();
+        if(substr_len>32)
+          assert(0);
+        if(rules[i].token_type==TK_NOTYPE)
+          break;
+        else{
+          tokens[nr_token].type=rules[i].token_type;
+          switch (rules[i].token_type) {
+            case TK_NUMBER:
+              strncpy(tokens[nr_token].str,substr_start,substr_len);
+              *(tokens[nr_token].str+substr_len)='\0';
+              break;
+            case TK_HEX:
+              strncpy(tokens[nr_token].str,substr_start+2,substr_len-2);
+              *(tokens[nr_token].str+substr_len-2)='\0';
+              break;
+            case TK_REG:
+              strncpy(tokens[nr_token].str,substr_start+1,substr_len-1);
+              *(tokens[nr_token].str+substr_len-1)='\0';
+              break;
+            default:
+              break;
+          }
+          nr_token++;
         }
-
         break;
       }
     }
@@ -97,13 +119,11 @@ static bool make_token(char *e) {
 }
 
 uint32_t expr(char *e, bool *success) {
+  *success = true;
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
 
   return 0;
 }
