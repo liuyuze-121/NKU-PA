8c5173de6218c50f604918ce06d74a28438a4c6e
tracer-ics2017 <tracer@njuics.org>
2026-04-15 10:37:31 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:37:31 up  4:22,  2 users,  load average: 0.13, 0.06, 0.01 f4415d398d9b72d2876290cde02141979ae973

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,9 +15,7 @@ typedef struct {
       uint8_t _8[2];
     } gpr[8];
 
-    struct {
-      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-    };
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
   };
 
   vaddr_t eip;
