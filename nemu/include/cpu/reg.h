306c6a3204b0f75d9361419ce9f2adb8df59cfa6
tracer-ics2017 <tracer@njuics.org>
2026-04-15 10:27:09 +0800

    >  compile 2411334 liuyuze Linux liuyuze-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:27:09 up  4:11,  2 users,  load average: 0.14, 0.07, 0.02 cec3f2a43f3ef324837e4de087d1ed5588509ab

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -9,12 +9,14 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
   union {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+    struct {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
 
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  };
 
   vaddr_t eip;
 
@@ -29,7 +31,7 @@ static inline int check_reg_index(int index) {
 
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
-#define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
+#define reg_b(index) (cpu.gpr[index >> 2]._8[index & 1])
 
 extern const char* regsl[];
 extern const char* regsw[];
